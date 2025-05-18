5a6b71384ec1e96e764d454a031066f8e879b522
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-05-18 14:05:49 +0300

    Add component OfferSection with animation: correspondence in messenger [OfferSection] (Content)

---
+++ src/sections/OfferSection/index.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react"
+import styles from "./offerSection.module.css"
+import Button from "../../components/Button"
+import cn from "classnames-ts"
+
+const OfferSection: React.FC = () => {
+    const [scrolled, setScrolled] = useState<boolean>(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 3000) {
+                setScrolled(true)
+            } else {
+                setScrolled(false)
+            }
+        })
+    }, [scrolled])
+
+    return (
+        <div className={styles.offer}>
+            <div className={styles.wrap}>
+                <div className={styles.messageWrap}>
+                    <div
+                        className={cn(
+                            styles.message,
+                            scrolled && styles.messageOne
+                        )}
+                    >
+                        Я не знаю с чего начать ?
+                    </div>
+                    <div
+                        className={cn(
+                            styles.message,
+                            scrolled && styles.messageTwo
+                        )}
+                    >
+                        Пройди программу обучения на нашей площадке
+                    </div>
+                    <div
+                        className={cn(
+                            styles.message,
+                            scrolled && styles.messageThree
+                        )}
+                    >
+                        Выбери курс и оставь заявку на сайте. Администратор
+                        свяжеться с тобой и поможет определиться с направлением
+                        и дальнейшей траекторией развития
+                    </div>
+                </div>
+            </div>
+            <div className={styles.question}>
+                <div>
+                    <h2 className={styles.title}>Как превратить желания в</h2>
+                    <div className={styles.emphasis}> действия?</div>
+                </div>
+                <Button type="filled" className={styles.btn}>
+                    Выбрать курс
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default OfferSection
