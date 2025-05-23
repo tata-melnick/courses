42d004bfe4da0191215e7845589df91f6977ad6a
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-05-23 09:47:45 +0300

    Add component Footer [Footer] (FeedbackSection)

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import styles from "./footer.module.css"
+import Button from "../Button"
+import { RocketIcon } from "../../icons"
+
+const Footer: React.FC = () => {
+    return (
+        <div className={styles.footer}>
+            <div className={styles.info}>
+                <div className={styles.contact}>
+                    ИНН/ОГРН: 0245963841/1200200037773
+                </div>
+                <div className={styles.contact}>
+                    Телефон: +7 (999) 997-75-99
+                </div>
+                <div className={styles.contact}>E-mail: info@mismi.tech</div>
+            </div>
+            <div className={styles.info}>
+                <div className={styles.adress}>г. Москва, Проспект 2</div>
+                <div className={styles.copyright}>© 2025 ООО "КУРС"</div>
+                <Button link="#">Политика конфиденциальности</Button>
+            </div>
+
+            <Button link="#header" className={styles.btnRocket}>
+                <RocketIcon />
+            </Button>
+        </div>
+    )
+}
+
+export default Footer
